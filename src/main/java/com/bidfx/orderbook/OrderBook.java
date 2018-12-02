5dc66272b2ada68f66bc2b11067505e21faaa9e5
Jean Francois Pascal <jeffpascal96@gmail.com>
2018-12-02 11:44:24 +0000

    Javadoc

--- src/main/java/com/bidfx/orderbook/OrderBook.java
+++ src/main/java/com/bidfx/orderbook/OrderBook.java
@@ -18,9 +18,19 @@ import java.util.TreeMap;
  */
 @SuppressWarnings("all")
 public class OrderBook {
+    
+    /** Represents the Delta after a remove/add operation
+    */
     private TreeMap<String, Object> updateDelta;
+    
+    /** Represents an orderBook
+     */
     public ArrayList<Order> orderBook = new ArrayList<Order>();
 
+    
+    /** Adds an order to orderBook
+     * @param order The order to be added
+    */
     public void add(Order order) {
         updateDelta = new TreeMap<String, Object>();
         if (orderBook.size() == 0) { // if the array is empty
@@ -28,55 +38,28 @@ public class OrderBook {
             updateDelta.put("Bid1", order.getPrice());
             updateDelta.put("BidSize1", order.getSize());
         } else// other 3 cases
-            addIfNotFirst(order); //method call to add if the order is not the first one to be added
+            addIfNotFirst(order); // method call to add if the order is not the first one to be added
     }
 
+    /** Removes an order from orderBook
+     * @param order The order to be removed
+    */
     public void remove(Order order) {
         updateDelta = new TreeMap<String, Object>();
-        for (int i = 0; i < orderBook.size(); i++) {
-            if (orderBook.get(i).getOrderId() == order.getOrderId()) {
-                if (i == orderBook.size() - 1) { // if we're removing last element
-                    long sizeIfPriceEqual = 0;
-                    orderBook.remove(i);
-                    for (int j = 0; j < i; j++) {
-                        if (orderBook.get(j).getPrice() == order.getPrice())
-                            sizeIfPriceEqual = sizeIfPriceEqual + orderBook.get(j).getSize();
-                    }
-                    if (sizeIfPriceEqual > 0) {
-                        updateDelta.put("BidSize".concat(String.valueOf(i)), sizeIfPriceEqual);
-                    } else {
-                        updateDelta.put("BidSize".concat(String.valueOf(i + 1)), null);
-                        updateDelta.put("Bid".concat(String.valueOf(i + 1)), null);
-                    }
-                    // format test requirement if element to remove is last
-
-                    return;
-                } else // any other case
-                {
-                    
-                    for (int j = i + 1; j < orderBook.size(); j++) {
-                        // format for every element present after removed item
-                        updateDelta.put("BidSize".concat(String.valueOf(j)), orderBook.get(j).getSize());
-                        updateDelta.put("Bid".concat(String.valueOf(j)), orderBook.get(j).getPrice());
-
-                    }
-                    long sizeIfPriceEqual = 0;
-                    for (int j = 0; j < i; j++) { // make sure Size updateDelta is right
-                        if (orderBook.get(j).getPrice() == order.getPrice())
-                            sizeIfPriceEqual = sizeIfPriceEqual + orderBook.get(j).getSize();
-                    }
-                    if (sizeIfPriceEqual > 0) {
-                        updateDelta.put("BidSize".concat(String.valueOf(i)), sizeIfPriceEqual);
-                    }
-                    orderBook.remove(i);
-                }
-
-            }
-
-        }
+        for (int i = 0; i < orderBook.size(); i++)
+            if (orderBook.get(i).getOrderId() == order.getOrderId())
+                if (i == orderBook.size() - 1) // if we're removing last element
+                    removeIfLast(i, order); // remove if order is the last order
+                else // any other case
+                    removeIfNotLast(i, order); // remove if order is not last order
 
     }
 
+    /**
+     * Adds {@code Order} if it is not the first one
+     * 
+     * @param order Order to be added
+     */
     private void addIfNotFirst(Order order) {
         for (int i = 0; i < orderBook.size(); i++) {
             if (orderBook.get(i).getPrice() == order.getPrice()) { // CASE IF WE HAVE EQUAL NEW PRICE SHARE
@@ -111,7 +94,59 @@ public class OrderBook {
 
         }
     }
+    
+    /**
+     * Removes order at index given if it is the last one
+     * 
+     * @param indexToRemove the index of the order to be removed
+     * @param order Order to be removed
+     */
+    public void removeIfLast(int indexToRemove, Order order) {
+        long sizeIfPriceEqual = 0;
+        orderBook.remove(indexToRemove);
+        for (int j = 0; j < indexToRemove; j++) {
+            if (orderBook.get(j).getPrice() == order.getPrice())
+                sizeIfPriceEqual = sizeIfPriceEqual + orderBook.get(j).getSize();
+        }
+        if (sizeIfPriceEqual > 0) {
+            updateDelta.put("BidSize".concat(String.valueOf(indexToRemove)), sizeIfPriceEqual);
+        } else {
+            updateDelta.put("BidSize".concat(String.valueOf(indexToRemove + 1)), null);
+            updateDelta.put("Bid".concat(String.valueOf(indexToRemove + 1)), null);
+        }
+        // format test requirement if element to remove is last
 
+        return;
+    }
+    
+    /**
+     * Removes order at index given if it is not the last one
+     * 
+     * @param indexToRemove the index of the order to be removed
+     * @param order Order to be removed
+     */
+    private void removeIfNotLast(int indexToRemove, Order order) {
+        for (int j = indexToRemove + 1; j < orderBook.size(); j++) {
+            // format for every element present after removed item
+            updateDelta.put("BidSize".concat(String.valueOf(j)), orderBook.get(j).getSize());
+            updateDelta.put("Bid".concat(String.valueOf(j)), orderBook.get(j).getPrice());
+
+        }
+        long sizeIfPriceEqual = 0;
+        for (int j = 0; j < indexToRemove; j++) { // make sure Size updateDelta is right
+            if (orderBook.get(j).getPrice() == order.getPrice())
+                sizeIfPriceEqual = sizeIfPriceEqual + orderBook.get(j).getSize();
+        }
+        if (sizeIfPriceEqual > 0) {
+            updateDelta.put("BidSize".concat(String.valueOf(indexToRemove)), sizeIfPriceEqual);
+        }
+        orderBook.remove(indexToRemove);
+
+    }
+
+    /** Return the delta (the changes)
+     * @return The updated delta variable
+    */
     public Map<String, Object> getChangedLevels() {
         // TODO Auto-generated method stub
 
